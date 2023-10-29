41820293799fc9c00a77108dfb6be87b0558c7ef
HoneySafonovets <kfs.boi@yandex.ru>
2023-10-30 00:07:16 +0300

    feat: complete task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,25 @@
 class GuessingGame {
-    constructor() {}
-
-    setRange(min, max) {
-
-    }
-
-    guess() {
-
-    }
-
-    lower() {
-
-    }
-
-    greater() {
-
-    }
+    constructor() {
+        // this.number = number;
+        this.mid;
+      }
+    
+      setRange(min, max) {
+        this.min = min;
+        this.max = max;
+      }
+    
+      guess() {
+          return this.mid = Math.ceil((this.max + this.min) / 2);
+      }
+    
+      lower() {
+        return this.max = this.mid;
+      }
+    
+      greater() {
+        return this.min = this.mid;
+      }
 }
 
 module.exports = GuessingGame;
